f88ca1a52d470d73c58be2819aed93b603b09de8
andreww99982-code <andreww99982@gmail.com>
2026-04-03 12:57:44 +0500

    Add bot configuration

---
+++ bot/config.php
@@ -0,0 +1,15 @@
+<?php
+// Telegram Bot Token
+const BOT_TOKEN = '8616709517:AAHylK6CeYxjaOMVSAe3DMaJ_-yOC7zOoSY';
+// Telegram Username
+const BOT_USERNAME = 'selldocs_roBot';
+// Admin Telegram ID
+const ADMIN_TELEGRAM_ID = '1415541259';
+// Admin Login Credentials
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = 'Azaza123@';
+// Site URL
+const SITE_URL = 'https://churchills-war-rooms-tickets.click';
+// Webhook URL
+const WEBHOOK_URL = 'https://churchills-war-rooms-tickets.click/bot/webhook.php';
+?>
